88e0fec9dd28a000c183bd88d78356356d6b6735
Mark Eschbach <meschbach@gmail.com>
2017-06-04 10:40:49 -0700

    Initial revision of test service

--- test-service/service.js
+++ test-service/service.js
@@ -1,6 +1,10 @@
 let express = require("express")
 let application = express()
 
+application.get( "/proxy-test/received", (request,response) => {
+	response.json( {passed: true })
+})
+
 let listener = application.listen( 0, () => {
-	console.log( listener.address().port )
+	console.log( "http://localhost:" +listener.address().port )
 })
