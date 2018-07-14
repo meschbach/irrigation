e569c840642452c047bae985e3172d3ffbd6ac18
Mark Eschbach <meschbach@gmail.com>
2018-07-14 16:17:33 -0700

    [utils/server socket URL] Promises URL for future

--- express-extensions.js
+++ express-extensions.js
@@ -5,18 +5,18 @@
  */
 
 // Internal dependencies
-let defer = require( './defer' )
+const Future = require( 'junk-bucket/Future' );
 
 //TOOD: Can these extened the actual application instances?
 /*
  * Promises the listening URL for the application service
  */
 exports.promise_listening_url = function promise_express_listening_url( app, port ){
-	return defer( ( resolve, reject ) => {
-		let listener = app.listen( port, () => {
-			let url = "http://localhost:" + listener.address().port
-			resolve( url )
-		})
-	})
+	const result = Future();
+	const listener = app.listen( port, () => {
+		let url = "http://localhost:" + listener.address().port
+		result.resolve( url );
+	});
+	return result.promised;
 }
 
