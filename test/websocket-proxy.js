e493c80a4cd445c80fea5a137b24207c3ebeb4cc
Mark Eschbach <meschbach@gmail.com>
2018-07-23 15:26:42 -0700

    [proxy/sockets] Verify web proxies may be proxied

--- test/websocket-proxy.js
+++ test/websocket-proxy.js
@@ -6,15 +6,23 @@ const {DeltaClient} = require("../client");
 let delta = require( "../index" )
 
 const {promiseEvent} = require("junk-bucket/future");
+const Future = require("junk-bucket/future");
 
 describe( "When configuring an ingress for websockets", function() {
-	before(async function(){
+	beforeEach(async function(){
 		const service = new WebSocket.Server({port:0});
+		this.targetMessage = new Future();
 		service.on('connection', (ws) => {
 			console.log("Connected");
 			this.connected = true;
+			ws.send("connected");
 			ws.on('message', (m) => {
+				console.log("message");
+				if( !this.targetMessage.resolved ) {
+					this.targetMessage.accept( m );
+				}
 				this.serviceMessage = m;
+				ws.send("ack");
 			})
 		});
 		await promiseEvent( service, "listening" );
@@ -30,7 +38,7 @@ describe( "When configuring an ingress for websockets", function() {
 		this.wsIngressURL = "ws://localhost:"+ingressAddress.port;
 		this.ingress.targets.push("target");
 	});
-	after(function(){
+	afterEach(function(){
 		this.proxy.stop();
 		this.targetService.close();
 	});
@@ -43,6 +51,24 @@ describe( "When configuring an ingress for websockets", function() {
 		expect(this.connected).to.be.true;
 	});
 
-	it( "allows for the client to send the server a message" );
-	it( "allows the server to send the client a message" );
+	it( "allows for the client to send the server a message", async function(){
+		const url = this.wsIngressURL;
+		console.log("Proxy url: " + url);
+		const ws = new WebSocket(url);
+		await promiseEvent(ws, "open");
+		const message = 'test';
+		ws.send(message);
+		await this.targetMessage.promised;
+		expect(this.serviceMessage).to.be.eq(message);
+	} );
+
+	it( "allows the server to send the client a message", async function(){
+		const url = this.wsIngressURL;
+		console.log("Proxy url: " + url);
+		const ws = new WebSocket(url);
+		const resultPromise = promiseEvent(ws, "message")
+		await promiseEvent(ws, "open");
+		const result = await resultPromise
+		expect(result).to.be.eq("connected");
+	} );
 });
