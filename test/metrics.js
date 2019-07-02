7da0df2080e2f1f50da64b32799b1da80677a62f
Mark Eschbach <meschbach@gmail.com>
2019-07-01 19:14:11 -0700

    metrics: Different method to assert recording of metrics

--- test/metrics.js
+++ test/metrics.js
@@ -3,18 +3,25 @@ const {expect}  = require("chai");
 const {delay} = require("junk-bucket/future");
 const {newMetricsPlatform, promiseMetric} = require("../junk");
 
+function totalSampleCount( registry, metric ){
+	const records = registry.getSingleMetric( metric );
+	const buckets = records.hashMap[''].bucketValues;
+	const values =  buckets['5'] + buckets['10'];
+	return values;
+}
+
 describe("Metrics", function(){
 	it( "records elapsed time", async function(){
 		const platform = newMetricsPlatform();
 		const observer = platform.measure("test", {examle:"point"});
-		await delay(6);
+		await delay(7);
 		observer.done();
-		expect(platform.registry.getSingleMetric("test").hashMap[''].bucketValues[10]).to.eq(1);
+		expect(totalSampleCount(platform.registry, "test")).to.eq(1);
 	});
 
 	it("will track a promise", async function() {
 		const platform = newMetricsPlatform();
-		await platform.promise("promise_test", {}, delay(5));
-		expect(platform.registry.getSingleMetric("promise_test").hashMap[''].bucketValues[10]).to.eq(1);
+		await platform.promise("promise_test", {}, delay(7));
+		expect(totalSampleCount(platform.registry, "promise_test")).to.eq(1);
 	});
 });
